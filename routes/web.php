440a59a9c3bb0ccc7b8a2c96e1ac5641b8331e01
tjanuki <t.januki@gmail.com>
2024-09-30 16:19:29 -0400

    Try a defer function

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/', function () {
+
+    defer(function () {
+        sleep(3);
+        logger('3 seconds later');
+    });
+    return 'Submitted';
+});
+
